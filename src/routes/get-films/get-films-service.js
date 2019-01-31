7533cc7920348e9e18d926427b27db0556353263
Jeevan gill <jsg17@hotmail.co.uk>
2019-01-31 22:01:01 +0000

    endpoint to get film info by id

--- src/routes/get-films/get-films-service.js
+++ src/routes/get-films/get-films-service.js
@@ -16,6 +16,35 @@ router.get("/", (req, res) => {
         });
 });
 
+router.get("/:filmId", (req, res) => {
+    const filmId = req.params.filmId;
+
+    if (isNaN(filmId)) {
+        res.status(400).json({ error: { status: 400, message: "Id must be a number" } })
+    } else {
+        rp.get(`${process.env.MOVIE_DB_API_URL}movie/${filmId}?api_key=${process.env.MOVIE_DB_API_KEY}&language=en-US`)
+        .then((filmData) => {
+            if(filmData.status_code !== 6) {
+                const data = JSON.parse(filmData);
+                const formattedData = {
+                    name: data.title,
+                    rating: data.vote_average,
+                    amountOfRatings: data.vote_count,
+                    runtime: data.runtime,
+                    date: data.release_date,
+                    summary: data.overview,
+                    imdbTitle: data.imdb_id,
+                    genres: data.genres.map((genre) => genre.name),
+                    imgUrl: data.belongs_to_collection.post_path
+                }
+                res.json(formattedData);
+            } else {
+                res.status(400).json({ error: { status: 400, message: `Film with id ${filmId} not found`  } });
+            }
+        });
+    }
+});
+
 router.get("/ping", async (req, res) => {
     res.send("pong " + req.userId);
 });
