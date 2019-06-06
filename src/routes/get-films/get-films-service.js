f0016c1aa1a6c0f5673701c32ef36b6d2d6a4466
Jeevan gill <jsg17@hotmail.co.uk>
2019-06-05 17:52:11 -0700

    Sometimes image url doesn't return

--- src/routes/get-films/get-films-service.js
+++ src/routes/get-films/get-films-service.js
@@ -22,10 +22,21 @@ router.get("/:filmId", (req, res) => {
     if (isNaN(filmId)) {
         res.status(400).json({ error: { status: 400, message: "Id must be a number" } })
     } else {
+        console.log(`triggered ${process.env.MOVIE_DB_API_URL}`);
         rp.get(`${process.env.MOVIE_DB_API_URL}movie/${filmId}?api_key=${process.env.MOVIE_DB_API_KEY}&language=en-US`)
         .then((filmData) => {
             if(filmData.status_code !== 6) {
                 const data = JSON.parse(filmData);
+                let theImage;
+                
+                if(data && data.belongs_to_collection && data.belongs_to_collection.poster_path) {
+                    theImage = data.belongs_to_collection.poster_path;
+                } else if(data && data.backdrop_path) {
+                    theImage = data.backdrop_path;
+                } else {
+                    theImage = null;
+                }
+
                 const formattedData = {
                     name: data.title,
                     rating: data.vote_average,
@@ -34,8 +45,8 @@ router.get("/:filmId", (req, res) => {
                     date: data.release_date,
                     summary: data.overview,
                     imdbTitle: data.imdb_id,
+                    imgUrl: theImage,
                     genres: data.genres.map((genre) => genre.name),
-                    imgUrl: data.belongs_to_collection.poster_path,
                     film_api_key: filmData.id
                 }
                 res.json(formattedData);
