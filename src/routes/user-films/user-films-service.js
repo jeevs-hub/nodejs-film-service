d53a409accc253371442b4b8841149e1e3a306fc
Jeevan gill <jsg17@hotmail.co.uk>
2019-09-26 21:22:16 -0700

    update film endpoint

--- src/routes/user-films/user-films-service.js
+++ src/routes/user-films/user-films-service.js
@@ -15,8 +15,9 @@ router.get("/", async (req, res) => {
         const result = rows.map((r) => {
             r.film_details.watchByDate = r.watch_by;
             r.film_details.filmApiId = r.film_api_id;
-            r.film_details.imgUrl = r.film_details.photoUrl
-            r.film_details.id = r.id
+            r.film_details.imgUrl = r.film_details.photoUrl;
+            r.film_details.id = r.id;
+            r.name = r.film_name;
             return r.film_details;
         })
         res.send(result);
@@ -30,21 +31,20 @@ router.get("/", async (req, res) => {
 
 router.post("/addFilm", async (req, res) => {
     const { userId, body } = req;
-    const { data, film_api_id, watchByDate } = body;
+    const { data, film_api_id, watchByDate, filmName } = body;
+    console.log("add film ", userId, "     ", req);
     const filmExists = false;
     // const filmExists = await doesFilmExists(film_api_id, name);
-    
+
     if (filmExists) {
-        console.log("the film exists ", film_api_id, " name ", name);
+        console.log("the film exists ", film_api_id, " name ", filmName);
         res.status(400).send({ error: { status: 400, message: "Film Exists" } })
     } else {
         const client = await db.client();
         try {
             const filmId = uuidv4();
-            // await db.query(`insert into media_content(id, name, media_type, media_information, user_id, film_api_id) 
-            //                 values($1, $2, $3, $4, $5, $6)`, [filmId, name, 'FILM', data, userId, film_api_id]);
-            await db.query(`insert into films(id, film_details, watch_by, user_id, film_api_id) 
-                             values($1, $2, $3, $4, $5)`, [filmId, data, new Date(watchByDate), userId, film_api_id]);
+            await db.query(`insert into films(id, film_details, watch_by, user_id, film_api_id, film_name) 
+                             values($1, $2, $3, $4, $5, $6)`, [filmId, data, new Date(watchByDate), userId, film_api_id, filmName]);
             res.send(filmId);
         } catch (e) {
             console.log("error logging in ", e)
@@ -53,5 +53,22 @@ router.post("/addFilm", async (req, res) => {
             client.release();
         }
     }
+});
 
+router.put("/updateFilm", async (req, res) => {
+    const { userId, body } = req;
+    const { data, film_api_id, watchByDate, id } = body;
+    const filmExists = false;
+    // const filmExists = await doesFilmExists(film_api_id, name);
+    console.log("the film id ", id, " the user id ", userId);
+    const client = await db.client();
+    try {
+        const test = await db.query(`update films set film_details = $1, watch_by = $2 where id = $3 and user_id = $4`, [data, new Date(watchByDate), id, userId]);
+        res.send(test);
+    } catch (e) {
+        console.log("error logging in ", e)
+        res.send({ status: 500, message: `Something went wrong at our end.` })
+    } finally {
+        client.release();
+    }
 });
