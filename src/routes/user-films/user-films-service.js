11b9e501c5d61a22700a45a0995bbacb80da3f06
Jeevan gill <jsg17@hotmail.co.uk>
2019-10-01 20:08:07 -0700

    test

--- src/routes/user-films/user-films-service.js
+++ src/routes/user-films/user-films-service.js
@@ -2,6 +2,7 @@ const Router = require('express-promise-router');
 const router = new Router();
 const db = require("../../utils/db.js");
 const uuidv4 = require('uuid/v4');
+const rp = require('request-promise');
 const { doesFilmExists } = require("../../utils/helperMethods.js");
 
 module.exports = router;
@@ -10,16 +11,15 @@ module.exports = router;
 router.get("/", async (req, res) => {
     const { userId, query } = req;
     const { offset, limit, filter, order, orderAsc } = query;
-    console.log("the qry ", orderAsc)
     const client = await db.client();
     try {
-        const filterQuery = `(select * from films where user_id = $1 and (film_name like '%' || $4 || '%') offset $2 limit $3) 
+        // const filterQuery = `(select * from films where user_id = $1 and (film_name like '%' || $4 || '%') offset $2 limit $3) 
+        const filterQuery = `(select * from films where user_id = $1 offset $2 limit $3) 
         order by ${getOrderBy(order)} ${orderAsc === 'true' ? 'asc' : 'desc'}`;
-        console.log("filter query ", filterQuery)
         const countQuery = `(select count (*) from films where user_id = $1 and film_name like '%' || $2 || '%')`;
-        const  filterReq = db.query(filterQuery, [userId, offset, limit, filter]);
-        // const  filterReq = db.query(filterQuery, [userId, offset, limit, filter, order]);
-        const  countReq = db.query(countQuery, [userId, filter]);
+        const filterReq = db.query(filterQuery, [userId, offset, limit]);
+        // const filterReq = db.query(filterQuery, [userId, offset, limit, filter]);
+        const countReq = db.query(countQuery, [userId, filter]);
 
         let [filterResult, countResult] = await Promise.all([filterReq, countReq]);
         const { rows } = filterResult;
@@ -29,11 +29,11 @@ router.get("/", async (req, res) => {
             r.film_details.imgUrl = r.film_details.photoUrl;
             r.film_details.id = r.id;
             r.film_details.name = r.film_name;
+            r.film_details.date = r.release_date;
             return r.film_details;
         })
 
-        console.log("the c ", countResult)
-        res.send({data: result, count: countResult.rows[0].count});
+        res.send({ data: result, count: countResult.rows[0].count });
     } catch (e) {
         console.log("error logging in ", e)
         res.send({ status: 500, message: `Something went wrong at our end.` })
@@ -45,21 +45,16 @@ router.get("/", async (req, res) => {
 router.post("/addFilm", async (req, res) => {
     const { userId, body } = req;
     const { data, film_api_id, watchByDate, filmName } = body;
-    console.log("add film ", userId, "     ", req);
     const filmExists = false;
-    // const filmExists = await doesFilmExists(film_api_id, name);
-
     if (filmExists) {
         console.log("the film exists ", film_api_id, " name ", filmName);
         res.status(400).send({ error: { status: 400, message: "Film Exists" } })
     } else {
         const client = await db.client();
         try {
-            for (let i = 0; i < 100; i++) {
-                const filmId = uuidv4();
-                await db.query(`insert into films(id, film_details, watch_by, user_id, film_api_id, film_name) 
+            const filmId = uuidv4();
+            await db.query(`insert into films(id, film_details, watch_by, user_id, film_api_id, film_name) 
                              values($1, $2, $3, $4, $5, $6)`, [filmId, data, new Date(watchByDate), userId, film_api_id, `filmName - ${i}`]);
-            }
             res.send(filmId);
         } catch (e) {
             console.log("error logging in ", e)
@@ -74,7 +69,6 @@ router.put("/updateFilm", async (req, res) => {
     const { userId, body } = req;
     const { data, watchByDate, id } = body;
     const filmExists = false;
-    // const filmExists = await doesFilmExists(film_api_id, name);
     console.log("the film id ", id, " the user id ", userId);
     const client = await db.client();
     try {
@@ -88,13 +82,133 @@ router.put("/updateFilm", async (req, res) => {
     }
 });
 
+router.post("/addDummyData", async (req, res) => {
+    const { userId } = req;
+    const client = await db.client();
+    try {
+        for (let i = 0; i < 5; i++) {
+            rp.get(`${process.env.MOVIE_DB_API_URL}discover/movie?api_key=${process.env.MOVIE_DB_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${i + 1}`)
+                .then((x) => {
+                    x = JSON.parse(x);
+                    x.results.map(async (film) => {
+                        const date = new Date();
+                        date.setDate(date.getDate() + Math.floor((Math.random() * 100) + 1));
+                        const formattedFilm = {
+                            "genres": film.genre_ids.map((x) => convertGenreToId(x)),
+                            "rating": film.vote_average,
+                            "summary": film.overview,
+                            "photoUrl": film.poster_path ? film.poster_path : film.backdrop_path,
+                            "additionalNotes": "Test film"
+                        };
+                        const filmId = uuidv4();
+                        // console.log("the film ", formattedFilm)
+                        await db.query(`insert into films(id, film_details, watch_by, user_id, film_api_id, film_name, release_date) values($1, $2, $3, $4, $5, $6, $7)`, [filmId, formattedFilm, new Date(date), userId, film.id, film.title, new Date(film.release_date)]);
+                    })
+                })
+        }
+        res.send("Films added")
+    } catch (e) {
+        console.log("error adding dummy data ", e)
+        res.send({ status: 500, message: `Something went wrong at our end.` })
+    } finally {
+        client.release();
+    }
+});
 
 getOrderBy = (orderby) => {
     switch (orderby) {
-        case 'films':
+        case 'name':
             return 'film_name';
-        default: 
+        case 'runtime':
+            return 'runtime';
+        case 'releaseDate':
+            return 'release_date';
+        case 'watchByDate':
+            return 'watch_by';
+        case 'rating':
+            return 'rating';
+        default:
             return 'film_name';
 
     }
+}
+convertGenreToId = (genreId) => {
+    return [
+        {
+            "id": 28,
+            "name": "Action"
+        },
+        {
+            "id": 12,
+            "name": "Adventure"
+        },
+        {
+            "id": 16,
+            "name": "Animation"
+        },
+        {
+            "id": 35,
+            "name": "Comedy"
+        },
+        {
+            "id": 80,
+            "name": "Crime"
+        },
+        {
+            "id": 99,
+            "name": "Documentary"
+        },
+        {
+            "id": 18,
+            "name": "Drama"
+        },
+        {
+            "id": 10751,
+            "name": "Family"
+        },
+        {
+            "id": 14,
+            "name": "Fantasy"
+        },
+        {
+            "id": 36,
+            "name": "History"
+        },
+        {
+            "id": 27,
+            "name": "Horror"
+        },
+        {
+            "id": 10402,
+            "name": "Music"
+        },
+        {
+            "id": 9648,
+            "name": "Mystery"
+        },
+        {
+            "id": 10749,
+            "name": "Romance"
+        },
+        {
+            "id": 878,
+            "name": "Science Fiction"
+        },
+        {
+            "id": 10770,
+            "name": "TV Movie"
+        },
+        {
+            "id": 53,
+            "name": "Thriller"
+        },
+        {
+            "id": 10752,
+            "name": "War"
+        },
+        {
+            "id": 37,
+            "name": "Western"
+        }
+    ].find(x => x.id === genreId).name
 }
